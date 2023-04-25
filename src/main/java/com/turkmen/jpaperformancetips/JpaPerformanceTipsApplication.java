b2e46b1de0821b4d086e10197d5b3195528ac28d
tmustafad <tmustafad@gmail.com>
2023-04-25 13:55:55 +0200

    initial commit

---
+++ src/main/java/com/turkmen/jpaperformancetips/JpaPerformanceTipsApplication.java
@@ -0,0 +1,13 @@
+package com.turkmen.jpaperformancetips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaPerformanceTipsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaPerformanceTipsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/turkmen/jpaperformancetips/JpaPerformanceTipsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turkmen.jpaperformancetips;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaPerformanceTipsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
